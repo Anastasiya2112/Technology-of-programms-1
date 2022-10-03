004e229fa21e8eaefd17822f66acb050273d63a9
Anastasiya P <anastasiyaap18@gmail.com>
2022-10-04 02:39:13 +0800

    Внесены все задания

---
+++ Задание 2. Лаба 1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Задание_2._Лаба_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int[] { 2, -16, 0, 4, -1 };
+            int max_elem1 = 0, maximum = 0, minimum = 0, min_elem1 = 100, Indexmax1 = 0, Indexmax2 = 0;
+            for (int j = 0; j < A.Length; j++)
+            {
+                if (A[j] > max_elem1)
+                {
+                    max_elem1 = A[j];
+                    maximum = A[j];
+                    Indexmax1 = j;
+                }
+
+                if (A[j] < min_elem1)
+                {
+                    min_elem1 = A[j];
+                    minimum = A[j];
+                    Indexmax2 = j;
+                }
+            }
+            Console.WriteLine("Максимальный элемент 1 = " + maximum);
+            Console.WriteLine("Минимальный элемент 1 = " + minimum);
+            max_elem1 = -100;
+            for (int j = 0; j < A.Length; j++)
+            {
+                if (j == Indexmax1)
+                {
+                    continue;
+                }
+
+                else if (A[j] > max_elem1)
+                {
+                    max_elem1 = A[j];
+                    maximum = A[j];
+                }
+            }
+            min_elem1 = 100;
+            for (int j = 0; j < A.Length; j++)
+            {
+                if (j == Indexmax2)
+                {
+                    continue;
+                }
+                else if (A[j] < min_elem1)
+                {
+                    min_elem1 = A[j];
+                    minimum = A[j];
+                }
+            }
+            Console.WriteLine("Максимальный элемент 2 = " + maximum);
+            Console.WriteLine("Минимальный элемент 2 = " + minimum);
+//
+        }
+    }
+}
---
+++ лаба 1.3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace лаба_1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size, p, number = 0, symbols;
+            string TextLine = Console.ReadLine();
+            for (int j = 0; j < TextLine.Length; j++)
+            {
+                //symbols++;
+                if ((TextLine[j] >= '0') & (TextLine[j] <= '9'))
+                {
+                    number++;
+                }
+            }
+            size = TextLine.Length;
+            p = size - number;
+            Console.WriteLine("Доля в % букв в предложении = " + ((100 * p) / size));
+
+        }
+    }
+}
