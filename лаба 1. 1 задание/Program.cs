b77269e1051b71725e6dc32c8a7fcb9f7311ada6
Anastasiya P <anastasiyaap18@gmail.com>
2022-10-04 01:52:46 +0800

    Внесён код первого задания

---
+++ лаба 1. 1 задание/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace лаба_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///Составить программу нахождения произведения двух наименьших из трех различных чисел
+            int a, b, c, max;
+            Console.WriteLine("Введите a");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите b");
+            c = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите c");
+            b = int.Parse(Console.ReadLine());
+            if (a > b)
+            {
+                max = a;
+            }
+            else
+            {
+                max = b; ;
+            }
+            if (c>max)
+            {
+                max=c;
+            }
+            if (a == b || b ==c || a==c)
+            {
+                Console.WriteLine("Были введены одинаковые значения! Вычисление произведения не возможно");
+            }
+            else
+            {
+                Console.WriteLine("Произведениe двух наименьших из трех чисел = " + a * b * c / max);
+            }
+
+        }
+    }
+}
